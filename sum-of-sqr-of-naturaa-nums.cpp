1ab860f588b69355a99dac2366ac4c344f945933
aninarafath6 <arafathanin6@gmail.com>
2022-07-07 22:02:37 +0530

    documentaion

--- sum-of-sqr-of-naturaa-nums.cpp
+++ sum-of-sqr-of-naturaa-nums.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 int main()
 {
+
     int n;
     cout << "enter a number :";
+    // input a number from user
     cin >> n;
 
     int sum = 0;
+
+
     // using by for loop .
     for (int i = 1; i <= n; i++)
     {
         sum += i * i;
     }
 
-    // same as we can use by equation which is n *(n + 1) * (2*n + 1) / 6
+    //also we can use,  same as we can use by equation which is n *(n + 1) * (2*n + 1) / 6
+    // calculate sum squires of first n natural number.
     sum = n * (n + 1) * (2 * n + 1) / 6;
     cout << "sum is : " << sum;
 
